608b42c0225663c1e36d4f91832c5092121e93d4
Felipe A. Melo <70349910+felipeamelo@users.noreply.github.com>
2021-11-23 17:01:44 +0000

    Update api.js

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    //baseURL: 'http://192.168.0.101:3333',
-    baseURL: 'https://wwdriver-backend.herokuapp.com',
+    baseURL: 'http://192.168.0.101:3333',
 })
 
 export default api;
